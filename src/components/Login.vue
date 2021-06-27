1cf33ba1a09e3bbd27bc8013a852e9ada3c3484a
pengminmin <540965717@qq.com>
2021-06-27 18:18:32 +0800

    给表单添加验证规则

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,13 +6,13 @@
         <img src="../assets/logo.png" alt="">
       </div>
       <!-- 登录表单区域 -->
-      <el-form :model="loginForm" label-width="0px" class="login_form">
+      <el-form :model="loginForm" :rules="loginFormRules" label-width="0px" class="login_form">
         <!-- 用户名 -->
-        <el-form-item>
+        <el-form-item prop="username">
           <el-input v-model="loginForm.username" prefix-icon="iconfont icon-usercenter"></el-input>
         </el-form-item>
         <!-- 密码 -->
-        <el-form-item>
+        <el-form-item prop="password">
           <el-input v-model="loginForm.password" prefix-icon="iconfont icon-password" type="password"></el-input>
         </el-form-item>
         <!-- 按钮 -->
@@ -34,6 +34,37 @@ export default {
       loginForm: {
         username: 'zs',
         password: '123'
+      },
+      // 这是表单的验证规则
+      loginFormRules: {
+        // 验证用户名是否合法
+        username: [
+          {
+            required: true,
+            message: '请输入登录名称',
+            trigger: 'blur'
+          },
+          {
+            min: 3,
+            max: 10,
+            message: '长度在 3 到 10 个字符',
+            trigger: 'blur'
+          }
+        ],
+        // 验证密码是否合法
+        password: [
+          {
+            required: true,
+            message: '请输入登录密码',
+            trigger: 'blur'
+          },
+          {
+            min: 6,
+            max: 15,
+            message: '长度在 6 到 15 个字符',
+            trigger: 'blur'
+          }
+        ]
       }
     }
   }
