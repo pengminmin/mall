3fde468ba554518a2dad0b7492040843b4ec3d96
pengminmin <540965717@qq.com>
2021-06-27 22:41:48 +0800

    导航菜单的基本使用

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -11,7 +11,30 @@
     <!-- 页面主题区域 -->
     <el-container>
       <!-- 侧边栏 -->
-      <el-aside width='200px'>Aside</el-aside>
+      <el-aside width='200px'>
+        <!-- 侧边栏菜单区域 -->
+        <el-menu
+          background-color='#333744'
+          text-color='#fff'
+          active-text-color='#ffd04b'>
+          <!-- 一级菜单 -->
+          <el-submenu index='1'>
+            <!-- 一级菜单的模板区域 -->
+            <template slot='title'>
+              <i class='el-icon-location'></i>
+              <span>导航一</span>
+            </template>
+
+            <el-menu-item index='1-4-1'>
+              <template slot='title'>
+                <i class='el-icon-location'></i>
+                <span>导航一</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+
+        </el-menu>
+      </el-aside>
       <!-- 右侧内容主体 -->
       <el-main>Main</el-main>
     </el-container>
