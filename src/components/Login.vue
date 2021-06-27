616eca6293f460045c977c5b3373d1b849d3928e
pengminmin <540965717@qq.com>
2021-06-27 17:21:57 +0800

    自定义全局样式

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    登录组件
+  <div class="login_container">
+    <div class="login_box">
+
+    </div>
   </div>
 </template>
 
@@ -10,6 +12,20 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="less" scoped>
+.login_container {
+  background-color: #2b4b6b;
+  height: 100%;
+}
 
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
 </style>
