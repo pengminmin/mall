82ae1dd283bd205ff756c7fc44349da1aadc1580
pengminmin <540965717@qq.com>
2021-06-27 22:54:43 +0800

    获取左侧菜单数据

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -44,12 +44,29 @@
 <script>
 export default {
   name: 'Home',
+  data () {
+    return {
+      // 左侧菜单数据
+      menuList: []
+    }
+  },
+  created () {
+    this.getMenuList()
+  },
   methods: {
     logout () {
       // 清空 token
       window.sessionStorage.clear()
       // 跳转到登录页
       this.$router.push('/login')
+    },
+    // 获取所有的菜单
+    async getMenuList () {
+      const { data: res } = await this.$http.get('menus')
+      if (res.meta.status !== 200) {
+        return this.$message.error(res.meta.msg)
+      }
+      this.menuList = res.data
     }
   }
 }
