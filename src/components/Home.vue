ff37e18b7265b9034d07aaa25e8b1a2b4af24e62
pengminmin <540965717@qq.com>
2021-06-27 22:28:33 +0800

    头部布局

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,11 @@
 <template>
   <el-container class='home-container'>
     <!-- 头部区域 -->
-    <el-header>Header
+    <el-header>
+      <div>
+        <img src='../assets/logo.png' alt=''>
+        <span>电商后台管理系统</span>
+      </div>
       <el-button type='info' @click='logout'>退出</el-button>
     </el-header>
     <!-- 页面主题区域 -->
@@ -35,6 +39,26 @@ export default {
 
 .el-header {
   background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+
+  img {
+    width: 30px;
+    height: 30px;
+  }
+
+  > div {
+    display: flex;
+    align-items: center;
+
+    span {
+      margin-left: 15px;
+    }
+  }
 }
 
 .el-aside {
